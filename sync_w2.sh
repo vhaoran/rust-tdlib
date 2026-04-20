51f3704b3fea718d7df156606fc4ca7ecf818ae3
Weihaoran whr <whr@Weihaorans-Mac-Studio.local>
2026-04-20 18:56:19 +0800

    aaaa

--- sync_w2.sh
+++ sync_w2.sh
@@ -3,8 +3,8 @@
 
 rm -rf ./target
 
-sshpass -p ww ssh whr@w2 "cd /D d:\rust\; rmdir rust-tdlib /D /Q"
+ssh whr@w2 "cd /D d:\rust\; rmdir rust-tdlib /D /Q"
 
-sshpass -p ww scp -r ../rust-tdlib  whr@w2:/d:/rust/
+scp -r ../rust-tdlib  whr@w2:/d:/rust/
 
 
